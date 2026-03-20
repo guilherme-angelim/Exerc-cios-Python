9fadb42969f12072e124834bf9997255a1e0b75a
Guilherme <anglim.guilerme@gmail.com>
2026-03-20 14:53:58 -0300

    2-Realizando exercício 1

--- Exercícios/ex1.py
+++ Exercícios/ex1.py
@@ -2,4 +2,4 @@ valor = float(input("Digite o valor da compra: "))
 if valor > 100:
     print ("Sua compra terá promoção!")
 else:
-    print ("Sua compra não terá promoção!"
+    print ("Sua compra não terá promoção!")
