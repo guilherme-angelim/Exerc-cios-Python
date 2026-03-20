7e35bc5eb8686fcbdb803a6d4a73372ee231c2fc
Guilherme <anglim.guilerme@gmail.com>
2026-03-20 14:54:35 -0300

    3-Realizando exercício 2

---
+++ Exercícios/ex2.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+print (num1,"+",num2,"=",num1+num2,"\n")
+print (num1,"-",num2,"=",num1-num2,"\n")
+print (num1,"*",num2,"=",num1*num2,"\n")
+print (num1,"/",num2,"=",num1/num2)
