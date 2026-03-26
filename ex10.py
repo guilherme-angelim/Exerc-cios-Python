00f2b09f876d92b728687c81476dbc45a5c25b21
Guilherme <anglim.guilerme@gmail.com>
2026-03-26 14:22:10 -0300

    14- Realizando exercício 10

---
+++ ex10.py
@@ -0,0 +1,11 @@
+nota1 = float(input("Nota um: "))
+nota2 = float(input("Nota dois: "))
+nota3 = float(input("Nota três: "))
+media = (nota1*nota2*nota3)/3
+
+if media >=8:
+    print("Média geral: A")
+elif media >=5 and media<8:
+    print ("Média geral: B")
+else:
+    print ("Média geral: C")
