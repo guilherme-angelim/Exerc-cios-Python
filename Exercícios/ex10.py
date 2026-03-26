a2dd227b9526eea1f0452eb9f1f9565f103c8fe1
Guilherme <anglim.guilerme@gmail.com>
2026-03-26 14:26:11 -0300

    16-arrumando o calculo de média

--- Exercícios/ex10.py
+++ Exercícios/ex10.py
@@ -1,7 +1,7 @@
 nota1 = float(input("Nota um: "))
 nota2 = float(input("Nota dois: "))
 nota3 = float(input("Nota três: "))
-media = (nota1*nota2*nota3)/3
+media = (nota1+nota2+nota3)/3
 
 if media >=8:
     print("Média geral: A")
