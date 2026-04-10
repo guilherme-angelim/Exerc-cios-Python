1e800aa0d6b150b5f0fd7e68503b8da811e1e3e0
Guilherme <anglim.guilerme@gmail.com>
2026-04-10 09:21:45 -0300

    22-Realizando exercício 14

---
+++ Exercícios/ex14.py
@@ -0,0 +1,10 @@
+def somatoria (x,y):
+    return x+y
+resultado = somatoria(2,4)
+print ("A somatória dos números 2 e 4 é: ",resultado)
+
+
+
+# def somatoria(x,y):
+    # return x+y
+# print (somatoria(2,4))
