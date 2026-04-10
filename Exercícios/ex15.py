50aab4dbfd52e7dc455160bb10447225a9b96c97
Guilherme <anglim.guilerme@gmail.com>
2026-04-10 09:26:59 -0300

    23-Realizando exercício 15

---
+++ Exercícios/ex15.py
@@ -0,0 +1,8 @@
+def idadeV(x):
+    if x >= 18:
+        print ("O usuário é maior de idade")
+    else:
+        print ("O usuário é menor de idade")
+        
+idade = int(input("Digite sua idade: "))
+idadeV(idade)
